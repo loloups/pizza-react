4a9959be1aa40bdadfac85d1a56f7b02226a4985
yne <yne@users.noreply.github.com>
2018-01-17 13:58:48 +0100

    Split Into multiple classes

--- src/index.js
+++ src/index.js
@@ -61,33 +61,99 @@ class Board extends React.Component {
         );
     }
 }*/
-function addOrder(event){
-    fetch("//localhost:8080/order", {
-        method: "POST",
-        headers: new Headers({'Content-Type': 'application/json'}),
-        body: JSON.stringify([{name:event.target.name.value, price:event.target.price.value,ingredients:[]}])
-    }).then(res => console.log(res))
-    event.preventDefault();
-    return false;
+var server = "//172.20.0.188:8080";
+class Pizza extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name:"margarita",
+            price:4.5
+        };
+    }
 }
+class Order extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pizzas: [],
+            id : 0,
+            table: 42,
+        };
+    }
+}
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: [],
+        };
+    }
+            //pizzas: [{name:"margarita", price:4.5, count:0}],
+    //            <button onClick={this.getPizza.bind(this)}>Get Pizzas</button>
+
+    getPizza(){ // TODO : faire a l'init
+        fetch(server + "/pizza").then(j => j.json()).then(res => this.setState({pizzas : res}))
+    }
+    render(){
+        return (
+            <table><tbody>{this.state.pizzas.map(pizza => 
+                <tr key={pizza.name}>
+                <td>{pizza.name}</td>
+                <td>{pizza.count}</td>
+                <td><button onClick={this.addPizza(pizza)}>+</button></td>
+                </tr>
+            )}</tbody></table>
+        )  ;
+    }
+}
+
+
+
 class OrderList extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: [],
+        };
+    }
     getOrder(){
-        fetch("//localhost:8080/order").then(j => j.json()).then(res => this.list=res)
+        fetch(server + "/order").then(j => j.json()).then(res => this.setState({orders : res}))
+    }
+    addPizza(pizza){
+        var pizzas = this.state.pizzas.slice();
+        //pizzas.find(p => p.name == pizza.name).count++;
+        this.setState({pizzas: pizzas})
     }
     render(){
         return (
             <div>
-                <button onClick={this.getOrder}>List</button>
-                <ul>{this.list.map()}</ul>
+
+                <button onClick={this.getOrder.bind(this)}>Get Orders</button>
+                <h1>Orders:</h1>
+                <ul>{this.state.orders.map(order =>
+                    <li key={order.id}>{order.name}</li>
+                )}</ul>
             </div>
         )  ;
     }
 }
 class OrderFactory extends React.Component {
+    addOrder(event){
+        fetch(server + "/order", {
+            method: "POST",
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify([{name:event.target.name.value, price:event.target.price.value,ingredients:[]}])
+        }).then(res => console.log(res))
+        event.preventDefault();
+        return false;
+    }
     render(){
         return (
-            <form onSubmit={addOrder}><input defaultValue="calzone" name="name"/><input type="number" name="price" defaultValue="1"/><input type="submit"/></form>
+            <form onSubmit={this.addOrder}>
+                <input defaultValue="calzone" name="name"/>
+                <input type="number" name="price" defaultValue="1"/>
+                <input type="submit"/>
+            </form>
         )  ;
     }
 }
